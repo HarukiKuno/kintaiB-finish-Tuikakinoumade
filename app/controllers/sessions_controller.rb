ff57059e513f86227be8a8af25c5531702ac99b4
“HarukiKuno” <serasu123@gmail.com>
2019-05-25 04:05:15 +0000

    kanpeki 8

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,19 @@ class SessionsController < ApplicationController
   def new
   end
   
-  def create 
+  def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      redirect_to user
-      
+      redirect_back_or user
     else
       flash.now[:danger] = 'メールアドレスとパスワードの情報が一致しませんでした。'
-    render 'new'
+      render 'new'
     end
   end
 
-def destroy
-  log_out
-  redirect_to root_url
-end
+  def destroy
+    log_out
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,13 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy, :edit_basic_info, :update_basic_info]
   
-  
- def show
+ def index
+    @users = User.paginate(page: params[:page])
+ end
+
+  def show
     @user = User.find(params[:id])
   end
 
@@ -10,21 +16,76 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params) # この段階ではまだ実装は終わっていません。
+    @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = "ユーザーの新規作成に成功しました。"
       redirect_to @user
-      # 保存が成功した場合はここに記述した処理が実行される。
     else
       render 'new'
     end
   end
-  
-  private
-  
-  
-  def user_params
-    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+
+  def edit
   end
+
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "削除しました。"
+    redirect_to users_url
+  end
+  
+  def edit_basic_info
+  @user = User.find(params[:id])
+  end
+
+ def update_basic_info
+  @user = User.find(params[:id])
+  if @user.update_attributes(basic_info_params)
+    flash[:success] = "基本情報を更新しました。"
+    redirect_to @user   
+  else
+    render 'edit_basic_info'
+  end
+ end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+  end
+
+  def basic_info_params
+    params.require(:user).permit(:basic_time, :work_time)
+  end
+
+    # beforeアクション
+
+    # ログイン済みユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
