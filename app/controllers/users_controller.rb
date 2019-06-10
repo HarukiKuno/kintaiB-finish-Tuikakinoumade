68f460160aeebe08bff385fa6beb4ac341ed4e98
“HarukiKuno” <serasu123@gmail.com>
2019-06-10 13:14:12 +0000

    ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :show]
   before_action :correct_user,   only: [:edit, :update, :show]
-  before_action :admin_user,     only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :admin_user,     only: [:destroy, :edit_basic_info, :update_basic_info, :index]
   
  def index
     @users = User.paginate(page: params[:page])
