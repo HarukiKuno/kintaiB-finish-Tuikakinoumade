984e68783f31b179496b066f2d7d735964b915b6
“HarukiKuno” <serasu123@gmail.com>
2019-06-12 14:38:35 +0000

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
+  enum gender: { unknown: 0, male: 1, female: 2, other: 9 }
+  
+  
+  #ユーザー名による絞り込み 
+scope :get_by_name, ->(name) {
+where("name like ?", "%#{name}%")
+}
+#性別による絞り込み
+scope :get_by_gender, ->(gender) {
+where(gender: gender)
+}
+  
+  
   has_many :attendances, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
