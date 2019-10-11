146ac01f4205989edeca6cee8b3e005b793dad74
“HarukiKuno” <serasu123@gmail.com>
2019-10-11 12:43:25 +0000

    kanrisya 3nin tuika

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,28 @@ User.create!(name:  "管理者",
              password:              "password",
              password_confirmation: "password",
              admin: true)
+             
+             
+User.create!(name:  "上長A",
+             email: "serasu123@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)             
+             
+User.create!(name:  "上長B",
+             email: "serasu1234@gmail.com",
+             password:              "password",
+             password_confirmation: "password",
+             admin: true)
+             
+             
+User.create!(name:  "一般ユーザー",
+             email: "serasu12345@gmail.com",
+             password:              "password",
+             password_confirmation: "password")
+             
+             
+             
 
 59.times do |n|
   name  = Faker::Name.name
