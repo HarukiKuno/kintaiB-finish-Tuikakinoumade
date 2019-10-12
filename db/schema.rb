056c72ec0b2921577fa3c53120e11c29d7cd9bf7
“HarukiKuno” <serasu123@gmail.com>
2019-10-12 13:50:18 +0000

    karamu tuika

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190528124507) do
+ActiveRecord::Schema.define(version: 20191012134901) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -33,6 +33,12 @@ ActiveRecord::Schema.define(version: 20190528124507) do
     t.string "department"
     t.datetime "basic_time", default: "2019-02-19 22:30:00"
     t.datetime "work_time", default: "2019-02-19 23:00:00"
+    t.string "uid"
+    t.string "affiliation"
+    t.integer "employee_number"
+    t.datetime "basic_work_time"
+    t.datetime "designated_work_start_time"
+    t.datetime "designated_work_end_time"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
